d5e38c66b7feaaba9eb7972185692c4f30748ee4
Georg Brandl <georg@python.org>
2021-11-18 10:51:14 +0100

    Allow converting raw values to a type

--- examples/adstool.rs
+++ examples/adstool.rs
@@ -115,7 +115,8 @@ struct StateArgs {
 #[derive(StructOpt, Debug)]
 /// Raw read or write access for an indexgroup.
 enum RawAction {
-    /// Read some data from an index.
+    /// Read some data from an index.  Specify either --length (to print raw
+    /// bytes) or --type (to convert to a data type and print that).
     Read {
         /// the index group, can be 0xABCD
         #[structopt(parse(try_from_str = parse))]
@@ -124,8 +125,14 @@ enum RawAction {
         #[structopt(parse(try_from_str = parse))]
         index_offset: u32,
         /// the length, can be 0xABCD
-        #[structopt(parse(try_from_str = parse))]
-        length: usize,
+        #[structopt(long, parse(try_from_str = parse), required_unless = "type")]
+        length: Option<usize>,
+        /// the data type
+        #[structopt(long, required_unless = "length")]
+        r#type: Option<VarType>,
+        /// whether to print integers as hex
+        #[structopt(long)]
+        hex: bool
     },
     /// Write some data to an index.  Data is read from stdin.
     Write {
@@ -137,7 +144,7 @@ enum RawAction {
         index_offset: u32,
     },
     /// Write some data (read from stdin), then read data from an index.
-    Readwrite {
+    WriteRead {
         /// the index group, can be 0xABCD
         #[structopt(parse(try_from_str = parse))]
         index_group: u32,
@@ -145,13 +152,34 @@ enum RawAction {
         #[structopt(parse(try_from_str = parse))]
         index_offset: u32,
         /// the length to read, can be 0xABCD
-        #[structopt(parse(try_from_str = parse))]
-        read_length: usize,
+        #[structopt(parse(try_from_str = parse), required_unless = "read_type")]
+        read_length: Option<usize>,
+        /// the data type to interpret the read data as
+        #[structopt(long, required_unless = "read_length")]
+        read_type: Option<VarType>,
+        /// whether to print integers as hex
+        #[structopt(long)]
+        hex: bool
     },
+}
+
+#[derive(StructOpt, Debug)]
+/// Variable read or write access.
+struct VarArgs {
+    /// the variable type
+    r#type: VarType,
+    /// the variable name
+    name: String,
+    /// the new value, if given, to write
+    value: Option<String>,
+    /// whether to print integers as hex
+    #[structopt(long)]
+    hex: bool
 }
 
 #[derive(Clone, Copy, Debug, EnumString)]
 #[strum(serialize_all = "UPPERCASE")]
+// TODO put the type mapping stuff into the lib?
 enum VarType {
     Bool,
     Byte,
@@ -180,20 +208,6 @@ impl VarType {
             VarType::String => 255,
         }
     }
-}
-
-#[derive(StructOpt, Debug)]
-/// Variable read or write access.
-struct VarArgs {
-    /// the variable type
-    r#type: VarType,
-    /// the variable name
-    name: String,
-    /// the new value, if given, to write
-    value: Option<String>,
-    /// whether to print integers as hex
-    #[structopt(long)]
-    hex: bool
 }
 
 /// Target spec: IP plus optional AMS adress.
@@ -339,22 +353,34 @@ fn main_inner(args: Args) -> Result<(), Error> {
             let amsaddr = ads::AmsAddr::new(netid, amsport);
             let mut dev = ads::Client::new(tcp_addr, None, None)?.device(amsaddr);
             match subargs {
-                RawAction::Read { index_group, index_offset, length } => {
-                    let mut read_data = vec![0; length];
-                    dev.read(index_group, index_offset, &mut read_data)?;
-                    stdout().write_all(&read_data)?;
+                RawAction::Read { index_group, index_offset, length, r#type, hex } => {
+                    if let Some(length) = length {
+                        let mut read_data = vec![0; length];
+                        dev.read(index_group, index_offset, &mut read_data)?;
+                        stdout().write_all(&read_data)?;
+                    } else if let Some(typ) = r#type {
+                        let mut read_data = vec![0; typ.size()];
+                        dev.read(index_group, index_offset, &mut read_data)?;
+                        print_read_value(typ, &read_data, hex);
+                    }
                 }
                 RawAction::Write { index_group, index_offset } => {
                     let mut write_data = Vec::new();
                     stdin().read_to_end(&mut write_data)?;
                     dev.write(index_group, index_offset, &write_data)?;
                 }
-                RawAction::Readwrite { index_group, index_offset, read_length } => {
+                RawAction::WriteRead { index_group, index_offset, read_length, read_type, hex } => {
                     let mut write_data = Vec::new();
                     stdin().read_to_end(&mut write_data)?;
-                    let mut read_data = vec![0; read_length];
-                    dev.write_read(index_group, index_offset, &write_data, &mut read_data)?;
-                    stdout().write_all(&read_data)?;
+                    if let Some(length) = read_length {
+                        let mut read_data = vec![0; length];
+                        dev.write_read(index_group, index_offset, &write_data, &mut read_data)?;
+                        stdout().write_all(&read_data)?;
+                    } else if let Some(typ) = read_type {
+                        let mut read_data = vec![0; typ.size()];
+                        dev.write_read(index_group, index_offset, &write_data, &mut read_data)?;
+                        print_read_value(typ, &read_data, hex);
+                    }
                 }
             }
         }
@@ -365,78 +391,86 @@ fn main_inner(args: Args) -> Result<(), Error> {
             let amsaddr = ads::AmsAddr::new(netid, amsport);
             let mut dev = ads::Client::new(tcp_addr, None, None)?.device(amsaddr);
             let mut handle = ads::symbol::Handle::new(&mut dev, &subargs.name)?;
+            let typ = subargs.r#type;
 
+            // Write or read data?
             if let Some(value) = subargs.value {
-                // Write data
-                let err = |_| Error::Str("expected integer".into());
-                let float_err = |_| Error::Str("expected floating point number".into());
-                let buf = match subargs.r#type {
-                    VarType::String => value.into_bytes(),
-                    VarType::Bool => {
-                        if value == "TRUE" {
-                            vec![1]
-                        } else if value == "FALSE" {
-                            vec![0]
-                        } else {
-                            return Err(Error::Str("invalid BOOL value".into()));
-                        }
-                    }
-                    VarType::Byte  => parse::<u8>(&value).map_err(err)?.to_le_bytes().into(),
-                    VarType::Sint  => parse::<i8>(&value).map_err(err)?.to_le_bytes().into(),
-                    VarType::Word  => parse::<u16>(&value).map_err(err)?.to_le_bytes().into(),
-                    VarType::Int   => parse::<i16>(&value).map_err(err)?.to_le_bytes().into(),
-                    VarType::Dword => parse::<u32>(&value).map_err(err)?.to_le_bytes().into(),
-                    VarType::Dint  => parse::<i32>(&value).map_err(err)?.to_le_bytes().into(),
-                    VarType::Lword => parse::<u64>(&value).map_err(err)?.to_le_bytes().into(),
-                    VarType::Lint  => parse::<i64>(&value).map_err(err)?.to_le_bytes().into(),
-                    VarType::Real  => value.parse::<f32>().map_err(float_err)?.to_le_bytes().into(),
-                    VarType::Lreal => value.parse::<f64>().map_err(float_err)?.to_le_bytes().into(),
-                };
-
-                handle.write(&buf)?;
+                let write_data = get_write_value(typ, value)?;
+                handle.write(&write_data)?;
             } else {
-                // Read data
-                let mut buf = vec![0; subargs.r#type.size()];
-                handle.read(&mut buf)?;
-                let value = match subargs.r#type {
-                    VarType::String => {
-                        println!("{}", String::from_utf8_lossy(&buf).split('\x00').next().unwrap());
-                        return Ok(());
-                    }
-                    VarType::Bool => {
-                        match buf[0] {
-                            0 => println!("FALSE"),
-                            1 => println!("TRUE"),
-                            n => println!("non-bool ({})", n)
-                        }
-                        return Ok(());
-                    }
-                    VarType::Byte  => buf[0] as i128,
-                    VarType::Sint  => buf[0] as i8 as i128,
-                    VarType::Word  => u16::from_le_bytes(buf[..2].try_into().unwrap()) as i128,
-                    VarType::Int   => i16::from_le_bytes(buf[..2].try_into().unwrap()) as i128,
-                    VarType::Dword => u32::from_le_bytes(buf[..4].try_into().unwrap()) as i128,
-                    VarType::Dint  => i32::from_le_bytes(buf[..4].try_into().unwrap()) as i128,
-                    VarType::Lword => u64::from_le_bytes(buf[..8].try_into().unwrap()) as i128,
-                    VarType::Lint  => i64::from_le_bytes(buf[..8].try_into().unwrap()) as i128,
-                    VarType::Real  => {
-                        let v = f32::from_le_bytes(buf[..4].try_into().unwrap());
-                        println!("{}", v);
-                        return Ok(());
-                    }
-                    VarType::Lreal => {
-                        let v = i64::from_le_bytes(buf[..8].try_into().unwrap());
-                        println!("{}", v);
-                        return Ok(());
-                    }
-                };
-                if subargs.hex {
-                    println!("{:#x}", value);
-                } else {
-                    println!("{}", value);
-                }
+                let mut read_data = vec![0; typ.size()];
+                handle.read(&mut read_data)?;
+                print_read_value(typ, &read_data, subargs.hex);
             }
         }
     }
     Ok(())
+}
+
+fn get_write_value(typ: VarType, value: String) -> Result<Vec<u8>, Error> {
+    let err = |_| Error::Str("expected integer".into());
+    let float_err = |_| Error::Str("expected floating point number".into());
+    Ok(match typ {
+        VarType::String => value.into_bytes(),
+        VarType::Bool => {
+            if value == "TRUE" {
+                vec![1]
+            } else if value == "FALSE" {
+                vec![0]
+            } else {
+                return Err(Error::Str("invalid BOOL value".into()));
+            }
+        }
+        VarType::Byte  => parse::<u8>(&value).map_err(err)?.to_le_bytes().into(),
+        VarType::Sint  => parse::<i8>(&value).map_err(err)?.to_le_bytes().into(),
+        VarType::Word  => parse::<u16>(&value).map_err(err)?.to_le_bytes().into(),
+        VarType::Int   => parse::<i16>(&value).map_err(err)?.to_le_bytes().into(),
+        VarType::Dword => parse::<u32>(&value).map_err(err)?.to_le_bytes().into(),
+        VarType::Dint  => parse::<i32>(&value).map_err(err)?.to_le_bytes().into(),
+        VarType::Lword => parse::<u64>(&value).map_err(err)?.to_le_bytes().into(),
+        VarType::Lint  => parse::<i64>(&value).map_err(err)?.to_le_bytes().into(),
+        VarType::Real  => value.parse::<f32>().map_err(float_err)?.to_le_bytes().into(),
+        VarType::Lreal => value.parse::<f64>().map_err(float_err)?.to_le_bytes().into(),
+    })
+}
+
+fn print_read_value(typ: VarType, buf: &[u8], hex: bool) {
+    let value = match typ {
+        VarType::String => {
+            println!("{}", String::from_utf8_lossy(buf).split('\x00').next().unwrap());
+            return;
+        }
+        VarType::Bool => {
+            match buf[0] {
+                0 => println!("FALSE"),
+                1 => println!("TRUE"),
+                n => println!("non-bool ({})", n)
+            }
+            return;
+        }
+        VarType::Real  => {
+            let v = f32::from_le_bytes(buf[..4].try_into().unwrap());
+            println!("{}", v);
+            return;
+        }
+        VarType::Lreal => {
+            let v = i64::from_le_bytes(buf[..8].try_into().unwrap());
+            println!("{}", v);
+            return;
+        }
+        VarType::Byte  => buf[0] as i128,
+        VarType::Sint  => buf[0] as i8 as i128,
+        VarType::Word  => u16::from_le_bytes(buf[..2].try_into().unwrap()) as i128,
+        VarType::Int   => i16::from_le_bytes(buf[..2].try_into().unwrap()) as i128,
+        VarType::Dword => u32::from_le_bytes(buf[..4].try_into().unwrap()) as i128,
+        VarType::Dint  => i32::from_le_bytes(buf[..4].try_into().unwrap()) as i128,
+        VarType::Lword => u64::from_le_bytes(buf[..8].try_into().unwrap()) as i128,
+        VarType::Lint  => i64::from_le_bytes(buf[..8].try_into().unwrap()) as i128,
+    };
+    // Only reaches here for integer types
+    if hex {
+        println!("{:#x}", value);
+    } else {
+        println!("{}", value);
+    }
 }
